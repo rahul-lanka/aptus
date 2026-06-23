87cf15548700fbf415d8b5a6a197a501c9013dcf
Rahul-Chimple <lankarahul121@gmail.com>
2026-06-24 00:13:08 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react],
+  base: "/aptus/",
+  plugins: [react()],
   optimizeDeps: {
-    include: ["pdfjs-dist"]
-  }
+    include: ["pdfjs-dist"],
+  },
 });
